d5d2df0a3de6c7286967a066b5460199b8adeae8
Pat Keeler <keelerpl@gmail.com>
2015-03-18 19:08:20 -0500

    Update Sql test files.

--- SqlTestData/Sql_06.sql
+++
@@ -1,3 +0,0 @@
-select c.nm as name, a.add as address, (select c.id from c where c.nm = 'pat'), p.ph as phone
-from customer c, address a, phone p
-where c.id = a.id and c.id = p.id and c.id in (select c.id from c where c.nm = 'pat') ;
