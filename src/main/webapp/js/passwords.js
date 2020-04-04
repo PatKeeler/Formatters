df7f469ec9dc588ca8b3201d32e5ed5c7f13cf19
PatKeeler <keelerpl@gmail.com>
2020-04-04 10:51:55 -0600

    Changed comments and layout in passwords js file

--- src/main/webapp/js/passwords.js
+++ src/main/webapp/js/passwords.js
@@ -13,12 +13,9 @@ var PasswordImage = [
             "0","1","2","3","4","5","6","7","8","9",
             "A","B","C","D","E","F",
 
-            // 36 chars - Numbers and upper case letters
-            "G","H","I","J","K","L","M",
-            "N","O","P","Q","R","S","T",
-            "U","V","W","X","Y","Z",
-
             // 62 chars - all but special characters
+            "G","H","I","J","K","L","M","N","O","P","Q","R","S",
+            "T","U","V","W","X","Y","Z",
             "a","b","c","d","e","f","g","h","i","j","k","l","m",
             "n","o","p","q","r","s","t","u","v","w","x","y","z",
 
